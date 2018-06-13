6b0bb591065eb58765d653a5093526d1d288aac6
Vinicius Teixeira <vinicius0026@gmail.com>
2018-06-12 22:47:27 -0300

    login page layout

--- assets/src/views/Login.vue
+++ assets/src/views/Login.vue
@@ -1,4 +1,39 @@
 <template lang="pug">
   .login-view
-    | login
+    v-container
+      v-layout
+        v-flex(xs12 sm6 offset-sm3)
+          v-card
+            v-card-title(primary-title)
+              div
+                h3.headline.mb-0 Login
+
+            v-card-text
+              v-form(ref="form")
+                v-text-field(
+                  v-model="username"
+                  label="Nome de usuário"
+                  required
+                )
+                v-text-field(
+                  v-model="password"
+                  label="Senha"
+                  type="password"
+                )
+
+            v-card-actions
+              v-spacer
+              v-btn Limpar
+              v-btn Login
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
