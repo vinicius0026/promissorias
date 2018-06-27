a9f28d80bdcd5788b1ebbd1cfd9a00c055afb073
Vinicius Teixeira <vinicius0026@gmail.com>
2018-06-26 22:54:48 -0300

    dealing with error messages that have parameters

--- assets/src/mixins/errorMessages.js
+++ assets/src/mixins/errorMessages.js
@@ -10,7 +10,18 @@ export default {
 
       const errorMessageTable = {
         required: 'Campo Obrigatório',
-        email: 'Email Inválido'
+        email: 'Email Inválido',
+        cpf: 'CPF Inválido',
+        minLength: {
+          message: 'Este campo deve ter ao menos {} caracteres',
+          errorKey: 'minLength',
+          paramKey: 'min'
+        },
+        maxLength: {
+          message: 'Este campo deve ter no máximo {} caracteres',
+          errorKey: 'maxLength',
+          paramKey: 'max'
+        }
       }
 
       return Object.keys(errorMessageTable)
@@ -19,7 +30,16 @@ export default {
           hasError: this.hasError(field, error)
         }))
         .filter(error => error.hasError)
-        .map(({ error }) => errorMessageTable[error])
+        .map(({ error }) => errorMessageTable[error] || 'Inválido')
+        .map(messageObj => {
+          if (typeof messageObj === 'string') {
+            return messageObj
+          }
+          return messageObj.message.replace(
+            '{}',
+            field.$params[messageObj.errorKey][messageObj.paramKey]
+          )
+        })
     }
   }
 }
