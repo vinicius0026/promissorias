75278c5a6da82a93d2020656bfe246d0799575fb
Vinicius Teixeira <vinicius0026@gmail.com>
2018-07-03 23:08:33 -0300

    relating promissory notes and installments on changeset

--- lib/promissorias/promissory_notes/promissory_note.ex
+++ lib/promissorias/promissory_notes/promissory_note.ex
@@ -2,9 +2,13 @@ defmodule Promissorias.PromissoryNotes.PromissoryNote do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Promissorias.PromissoryNotes.Installment
+
   schema "promissory_notes" do
     field(:amount, :decimal)
+
     belongs_to(:customer, Promissorias.Customers.Customer)
+    has_many(:installments, Installment, on_replace: :delete)
 
     timestamps()
   end
@@ -14,5 +18,6 @@ defmodule Promissorias.PromissoryNotes.PromissoryNote do
     promissory_note
     |> cast(attrs, [:amount])
     |> validate_required([:amount])
+    |> cast_assoc(:installments)
   end
 end
