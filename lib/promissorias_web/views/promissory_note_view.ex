31617a4bfe59cb4b5cc43deb11592d1e08c500f2
Vinicius Teixeira <vinicius0026@gmail.com>
2018-07-04 21:13:14 -0300

    returning customer and installments on promissory note view

--- lib/promissorias_web/views/promissory_note_view.ex
+++ lib/promissorias_web/views/promissory_note_view.ex
@@ -1,16 +1,24 @@
 defmodule PromissoriasWeb.PromissoryNoteView do
   use PromissoriasWeb, :view
   alias PromissoriasWeb.PromissoryNoteView
+  alias PromissoriasWeb.CustomerView
+  alias PromissoriasWeb.InstallmentView
 
   def render("index.json", %{promissory_notes: promissory_notes}) do
     %{data: render_many(promissory_notes, PromissoryNoteView, "promissory_note.json")}
   end
 
   def render("show.json", %{promissory_note: promissory_note}) do
+    IO.inspect(promissory_note)
     %{data: render_one(promissory_note, PromissoryNoteView, "promissory_note.json")}
   end
 
   def render("promissory_note.json", %{promissory_note: promissory_note}) do
-    %{id: promissory_note.id, amount: promissory_note.amount}
+    %{
+      id: promissory_note.id,
+      amount: promissory_note.amount,
+      customer: render_one(promissory_note.customer, CustomerView, "customer.json"),
+      installments: render_many(promissory_note.installments, InstallmentView, "installment.json")
+    }
   end
 end
